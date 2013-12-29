7373b3a9f5ae4342fa5ab6bed086116c6e1b8c1b
Michal Uherek <uherek.michal@gmail.com>
2013-12-29 21:36:35 +0100

    Middle of user signup

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131227204544) do
+ActiveRecord::Schema.define(version: 20131221201503) do
 
   create_table "users", force: true do |t|
     t.string   "name"
@@ -19,7 +19,6 @@ ActiveRecord::Schema.define(version: 20131227204544) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "password_digest"
-    t.string   "encrypted_password"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
